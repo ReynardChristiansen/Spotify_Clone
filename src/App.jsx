70704c9f32adbba87a43004a3fb9f448cf3e292e
ReynardChristiansen <reynard.satria@gmail.com>
2024-05-27 20:52:39 +0700

    fix style

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
 
   return (
     <div className='h-screen bg-black'>
-      <div className='h-[88%] flex'>
+      <div className='h-[90%] flex'>
         <Sidebar/>
         <Display/>
       </div>
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -54,7 +54,7 @@ const Player = () => {
   };
 
   return (
-    <div className='lg:h-[12%] sm:h-[15%] bg-black flex justify-between items-center text-white px-4'>
+    <div className='h-[10%] bg-black flex justify-between items-center text-white px-4'>
 
       <div className='hidden lg:flex items-center gap-4'>
         {track.image && <img className='w-12' src={track.image} alt='' />}
